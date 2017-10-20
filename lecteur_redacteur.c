b5ccab81355bc89cb7c03514cea9e48616a2cf32
PizzaSansAnchois <moijaimelapizza@gmail.com>
2017-10-20 08:54:56 +0200

    squelette du .c

---
+++ lecteur_redacteur.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include "lecteur_redacteur.h"
+
+
+void initialiser_lecteur_redacteur(lecteur_redacteur *){
+
+
+}
+
+
+void detruire_lecteur_redacteur(lecteur_redacteur *){
+
+
+}
+
+
+void debut_lecteur(lecteur_redacteur *){
+
+
+}
+
+
+void fin_lecteur(lecteur_redacteur *){
+
+
+}
+
+
+void debut_redaction(lecteur_redacteur *){
+
+
+}
+
+
+void fin_redaction(lecteur_redacteur *){
+
+
+}
+
