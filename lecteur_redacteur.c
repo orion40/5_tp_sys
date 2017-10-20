26b51c0dbd0beae696f11f0e0b06ec9084a8b567
whoami <whoami@hellsystem>
2017-10-20 09:51:38 +0200

    Correct declaration errors

--- lecteur_redacteur.c
+++ lecteur_redacteur.c
@@ -1,41 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
 #include "lecteur_redacteur.h"
 
-
-void initialiser_lecteur_redacteur(lecteur_redacteur *){
-
+void initialiser_lecteur_redacteur(lecteur_redacteur_t * lr){
 
 }
 
-
-void detruire_lecteur_redacteur(lecteur_redacteur *){
-
+void detruire_lecteur_redacteur(lecteur_redacteur_t * lr){
 
 }
 
-
-void debut_lecteur(lecteur_redacteur *){
-
+void debut_lecture(lecteur_redacteur_t * lr){
 
 }
 
-
-void fin_lecteur(lecteur_redacteur *){
-
+void fin_lecture(lecteur_redacteur_t * lr){
 
 }
 
-
-void debut_redaction(lecteur_redacteur *){
-
+void debut_redaction(lecteur_redacteur_t * lr){
 
 }
 
-
-void fin_redaction(lecteur_redacteur *){
-
+void fin_redaction(lecteur_redacteur_t * lr){
 
 }
-
--- lecteur_redacteur.h
+++ lecteur_redacteur.h
@@ -1,14 +1,13 @@
-#include <semaphore.h>
 #include <pthread.h>
 
 typedef struct {
     pthread_mutex_t mutex_lecture;
     pthread_mutex_t mutex_ecriture;
-} lecteur_redacteur;
+} lecteur_redacteur_t;
 
-void initialiser_lecteur_redacteur(lecteur_redacteur *);
-void detruire_lecteur_redacteur(lecteur_redacteur *);
-void debut_lecteur(lecteur_redacteur *);
-void fin_lecteur(lecteur_redacteur *);
-void debut_redaction(lecteur_redacteur *);
-void fin_redaction(lecteur_redacteur *);
+void initialiser_lecteur_redacteur(lecteur_redacteur_t *);
+void detruire_lecteur_redacteur(lecteur_redacteur_t *);
+void debut_lecture(lecteur_redacteur_t *);
+void fin_lecture(lecteur_redacteur_t *);
+void debut_redaction(lecteur_redacteur_t *);
+void fin_redaction(lecteur_redacteur_t *);
