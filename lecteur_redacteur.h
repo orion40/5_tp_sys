312d5b8de2771d97caaaf6aa88cb609533357e0e
whoami <whoami@hellsystem>
2017-10-20 09:34:51 +0200

    Add void

--- lecteur_redacteur.h
+++ lecteur_redacteur.h
@@ -1,6 +1,14 @@
-initialiser_lecteur_redacteur(lecteur_redacteur *);
-detruire_lecteur_redacteur(lecteur_redacteur *);
-debut_lecteur(lecteur_redacteur *);
-fin_lecteur(lecteur_redacteur *);
-debut_redaction(lecteur_redacteur *);
-fin_redaction(lecteur_redacteur *);
+#include <semaphore.h>
+#include <pthread.h>
+
+typedef struct {
+    pthread_mutex_t mutex_lecture;
+    pthread_mutex_t mutex_ecriture;
+} lecteur_redacteur;
+
+void initialiser_lecteur_redacteur(lecteur_redacteur *);
+void detruire_lecteur_redacteur(lecteur_redacteur *);
+void debut_lecteur(lecteur_redacteur *);
+void fin_lecteur(lecteur_redacteur *);
+void debut_redaction(lecteur_redacteur *);
+void fin_redaction(lecteur_redacteur *);
