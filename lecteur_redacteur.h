b6ef2cf9bd3c2ca4e7e54234df9644930321f16e
whoami <whoami@hellsystem>
2017-10-20 08:45:31 +0200

    Add lecteur_redacteur.*

---
+++ lecteur_redacteur.h
@@ -0,0 +1,6 @@
+initialiser_lecteur_redacteur(lecteur_redacteur *);
+detruire_lecteur_redacteur(lecteur_redacteur *);
+debut_lecteur(lecteur_redacteur *);
+fin_lecteur(lecteur_redacteur *);
+debut_redaction(lecteur_redacteur *);
+fin_redaction(lecteur_redacteur *);
