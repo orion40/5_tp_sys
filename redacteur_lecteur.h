4e2b9ae5361e0e91ed273fa00da5907ea83ecdcb
whoami <whoami@hellsystem>
2017-10-25 11:53:13 +0200

    Add version with redactor priority

---
+++ redacteur_lecteur.h
@@ -0,0 +1,18 @@
+#include <pthread.h>
+
+typedef struct {
+    int nb_redacteur_actif;
+    int nb_lecteur_actif;
+    int nb_lecteur_attente;
+    int nb_redacteur_attente;
+    pthread_mutex_t mutex;
+    pthread_cond_t notif_ecrivain;
+    pthread_cond_t notif_lecteur;
+} lecteur_redacteur_t;
+
+void initialiser_lecteur_redacteur(lecteur_redacteur_t *);
+void detruire_lecteur_redacteur(lecteur_redacteur_t *);
+void debut_lecture(lecteur_redacteur_t *);
+void fin_lecture(lecteur_redacteur_t *);
+void debut_redaction(lecteur_redacteur_t *);
+void fin_redaction(lecteur_redacteur_t *);
