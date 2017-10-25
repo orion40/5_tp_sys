eb118e409c1f8a824d36120460f05661d1da4bec
whoami <whoami@hellsystem>
2017-10-25 11:08:35 +0200

    Add function comments

--- lecteur_redacteur.c
+++ lecteur_redacteur.c
@@ -1,6 +1,11 @@
 #include "lecteur_redacteur.h"
 #include <stdio.h>
 
+/* fonction: initialiser_lecteur_redacteur
+ * Permet d'initialiser les structures d'un lecteur_redacteur_t
+ * correctement.
+ * lr : le lecteur_redacteur_t à initialiser.
+ */
 void initialiser_lecteur_redacteur(lecteur_redacteur_t * lr){
     lr->nb_redacteur_actif = 0;
     lr->nb_lecteur_actif = 0;
@@ -11,12 +16,20 @@ void initialiser_lecteur_redacteur(lecteur_redacteur_t * lr){
     pthread_cond_init(&lr->notif_lecteur, NULL);
 }
 
+/* fonction: detruire_lecteur_redacteur
+ * Permet de libérer les structures internes d'un lecteur_redacteur_t.
+ * lr : le lecteur_redacteur_t à libérer.
+ */
 void detruire_lecteur_redacteur(lecteur_redacteur_t * lr){
     pthread_mutex_destroy(&lr->mutex);
     pthread_cond_destroy(&lr->notif_lecteur);
     pthread_cond_destroy(&lr->notif_ecrivain);
 }
 
+/* fonction: debut_lecture
+ * Indique le début d'une lecture.
+ * lr : le lecteur_redacteur_t à utiliser.
+ */
 void debut_lecture(lecteur_redacteur_t * lr){
     pthread_mutex_lock(&lr->mutex);
     lr->nb_lecteur_attente++;
@@ -31,6 +44,10 @@ void debut_lecture(lecteur_redacteur_t * lr){
     pthread_mutex_unlock(&lr->mutex);
 }
 
+/* fonction: fin_lecture
+ * Indique la fin d'une lecture.
+ * lr : le lecteur_redacteur_t à utiliser.
+ */
 void fin_lecture(lecteur_redacteur_t * lr){
     pthread_mutex_lock(&lr->mutex);
 
@@ -43,6 +60,10 @@ void fin_lecture(lecteur_redacteur_t * lr){
     pthread_mutex_unlock(&lr->mutex);
 }
 
+/* fonction: debut_redaction
+ * Indique le début d'une rédaction.
+ * lr : le lecteur_redacteur_t à utiliser.
+ */
 void debut_redaction(lecteur_redacteur_t * lr){
     pthread_mutex_lock(&lr->mutex);
 
@@ -55,6 +76,10 @@ void debut_redaction(lecteur_redacteur_t * lr){
     pthread_mutex_unlock(&lr->mutex);
 }
 
+/* fonction: fin_redaction
+ * Indique la fin d'une rédaction.
+ * lr : le lecteur_redacteur_t à utiliser.
+ */
 void fin_redaction(lecteur_redacteur_t * lr){
     pthread_mutex_lock(&lr->mutex);
 
