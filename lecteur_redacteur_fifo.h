1456036fe07f342e666e4d0931df774cc3263806
orion40 <qolizion@protonmail.com>
2017-11-01 08:50:27 +0100

    Remove list prototypes

--- lecteur_redacteur_fifo.h
+++ lecteur_redacteur_fifo.h
@@ -9,16 +9,17 @@
 struct HEADNAME *headp;
 
 // TODO: besoin d'un type différent pour la tête de liste ?
-typedef struct {
-    struct list_elem *next;
+struct list_elem {
+    STAILQ_ENTRY(list_elem) next;
     pthread_cond_t signal;
     int nb_redacteur;
     int nb_lecteur;
     int est_lecteur;
-} list_elem;
+};
+typedef struct list_elem list_elem;
 
 typedef struct {
-    SLIST_HEAD(HEADNAME, list_elem) list;
+    STAILQ_HEAD(head_s, list_elem) list_head;
     pthread_mutex_t mutex;
 } lecteur_redacteur_t;
 
@@ -28,11 +29,5 @@ void debut_lecture(lecteur_redacteur_t *);
 void fin_lecture(lecteur_redacteur_t *);
 void debut_redaction(lecteur_redacteur_t *);
 void fin_redaction(lecteur_redacteur_t *);
-
-bool linked_list_is_empty(list_elem *list);
-void linked_list_init(list_elem *list);
-list_elem* linked_list_get_last_elem (list_elem *list);
-list_elem* linked_list_get_first_elem (list_elem *list);
-list_elem* linked_list_depiler (list_elem *list);
 
 #endif
